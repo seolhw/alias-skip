a39e2392a6358256bd12be5b3e9c289bc21ad4b7
前端李会旺 <45996179+seolhw@users.noreply.github.com>
2020-05-17 22:29:21 +0800

    高亮路径而不是单词

--- src/extension.ts
+++ src/extension.ts
@@ -13,19 +13,32 @@ const activate = function (context: vscode.ExtensionContext) {
       const fileName = document.fileName; // 当前文件的绝对路径加文件名
       const workDir = path.dirname(fileName); // 当前文件的绝对路径
       const linetext = document.lineAt(position).text; // 当前行字符串
-      const q = screeningPath(linetext) // 路由别名目标路径
+      const q = screeningPath(linetext, position) // 路由别名目标路径
       const z = rootPath(workDir, context) // 项目根目录
-      const u = screeningRelativePath(linetext) // 相对路径的目标路径
+      const u = screeningRelativePath(linetext,position) // 相对路径的目标路径
       let targetPath = '' // 要跳转的目标路径
+      let isPathInterior = false
+      let target = q
+
       if (q && z) {
-        targetPath = path.resolve(z, q)
+        targetPath = path.resolve(z, q.path)
+        isPathInterior = position.character >= q.columns[0] && position.character <= q.columns[1]
       } else if (u) {
-        targetPath = path.resolve(workDir, u)
+        targetPath = path.resolve(workDir, u.text)
+        isPathInterior = position.character >= u.columns[0] && position.character <= u.columns[1]
+        target = u
       }
       const k = joiningSuffix(targetPath) // 文件存在就返回目标文件，不存在就返回空字符串
-      if (!k) return
-      return new vscode.Location(vscode.Uri.file(k), new vscode.Position(0, 0))
-    }
+      if (!k || !isPathInterior) return
+      return [
+        {
+          originSelectionRange: target.rang,
+          targetRange: new vscode.Range(0,0,0,0),
+          // targetSelectionRange: new vscode.Range(0,0,0,10),
+          targetUri: vscode.Uri.file(k)
+        }
+      ]
+    },
   })
   context.subscriptions.push(hoverHander)
 }
--- src/utils.ts
+++ src/utils.ts
@@ -8,18 +8,24 @@ const { mappings, rootpath: rootfile, allowedsuffix } = (<any>vscode.workspace.g
  * @param {string} linetext 包含路径的字符串
  * @returns 目标路径
  */
-const screeningPath = function (linetext: string): string {
+const screeningPath = function (linetext: string, position: vscode.Position): any {
   let c = /('.+')|(".+")/
   let arr = linetext.match(c)
   if (arr) {
     let text = arr[0].substring(1, arr[0].length - 1)
+    const i = linetext.indexOf(text)
+    const columns = [i, i + text.length]
     let [key, ...m] = text.split('/')
     if (mappings.hasOwnProperty(key)) {
       let e = mappings[key]
       if (e[0] === '/') {
         e = e.substring(1)
       }
-      return path.join(e, ...m)
+      return {
+        path: path.join(e, ...m),
+        rang: new vscode.Range(position.line, columns[0], position.line, columns[1]),
+        columns
+      }
     }
   }
   return ''
@@ -83,13 +89,20 @@ const joiningSuffix = function (targetPath: string) {
  * @param {string} linetext 包含路径的字符串
  * @returns 目标路径的相对路径
  */
-const screeningRelativePath = function (linetext: string) {
+const screeningRelativePath = function (linetext: any,position: vscode.Position) {
   let arr = linetext.match(/('.+')|(".+")/) // 正则匹配
   let text = ''
   if (arr) {
     text = arr[0].substring(1, arr[0].length - 1)
+    const i = linetext.indexOf(text)
+    const columns = [i, i + text.length]
+    return {
+      text,
+      rang: new vscode.Range(position.line, columns[0], position.line, columns[1]),
+      columns
+    }
   }
-  return text
+  return ''
 }
 
 export {
